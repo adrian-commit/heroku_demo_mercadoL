8654575bfe51a54db1cc0d71ccd98ed4fa02c240
adrian-commit <santillanadrianalberto@hotmail.com>
2022-01-04 20:59:58 -0300

    app init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+
+app.use('/static', express.static(__dirname + '/public'));
+
+app.set('port', process.env.PORT || 3000)
+
+app.listen(app.get('port'), ()=> console.log('On http://localhost:'+app.get('port')));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{   
+    res.sendFile(__dirname + '/views/register.html');
+});
