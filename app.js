601b52297227e4800f4c07663c81e54fc23422e1
adrian-commit <santillanadrianalberto@hotmail.com>
2022-01-05 01:07:38 -0300

    cambios en config gral

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use('/static', express.static(__dirname + '/public'));
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000) 
 
 app.listen(app.get('port'), ()=> console.log('On http://localhost:'+app.get('port')));
 
